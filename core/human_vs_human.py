71fabaec4380dca130dce1e5382d8da93df5a9cf
Huazhong Ning <ninghz@sjc-huazhong-ning-mbp16-22.local>
2022-12-24 23:45:08 -0800

    human vs human play turns

--- core/human_vs_human.py
+++ core/human_vs_human.py
@@ -21,16 +21,21 @@ class HumanVsHuman(GameBase):
       player2_desc=player2_desc,
     )
     self.current_player = self.player1
+    self.in_click = False
 
   def onclick(self, x, y):
-    if self.game_over():
+    if self.game_over() or self.in_click:
       return
 
+    self.in_click = True
     i, j = self.board.mouse_point_to_grid_index(x, y)
     step_valid, win = self.step(i, j, self.current_player)
     if step_valid:
       self.current_player = self.opponent_player(self.current_player)
+    self.board.draw_turn(self.current_player)
 
     if self.game_over():
       print(self.game_over_text())
       self.draw_game_over_text()
+
+    self.in_click = False
